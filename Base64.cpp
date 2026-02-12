0577402bc741a85b3a0dac61bd344167d426f330
notproinprogramming <nikarsh7@gmail.com>
2026-02-12 20:56:23 +0200

    added decoder

--- Base64.cpp
+++ Base64.cpp
@@ -9,6 +9,16 @@ const std::string BASE64_ALPHABET =
     "abcdefghijklmnopqrstuvwxyz"
     "0123456789+/";
 
+int DECODE_TABLE[256];
+
+void initDecodeTable() {
+    for (int i = 0; i < 256; ++i)
+        DECODE_TABLE[i] = -1;
+
+    for (int i = 0; i < 64; ++i)
+        DECODE_TABLE[(unsigned char)BASE64_ALPHABET[i]] = i;
+}
+
 std::string encodeBase64(const std::vector<unsigned char>& data) {
     std::string result;
     int val = 0, valb = -6;
@@ -28,6 +38,140 @@ std::string encodeBase64(const std::vector<unsigned char>& data) {
         result.push_back('=');
     }
     return result;
+}
+
+bool decodeBase64Line(const std::string& line,
+                      std::vector<unsigned char>& output,
+                      int lineNumber,
+                      bool& paddingEncountered) {
+
+    int val = 0, valb = -8;
+    bool localPadding = false;
+
+    for (size_t i = 0; i < line.size(); ++i) {
+        unsigned char c = line[i];
+
+        if (c == '=') {
+            localPadding = true;
+            paddingEncountered = true;
+            continue;
+        }
+
+        if (localPadding) {
+            std::cerr << "Рядок " << lineNumber
+                      << ", символ " << i + 1
+                      << ": Неправильне використання паддінгу\n";
+            return false;
+        }
+
+        if (DECODE_TABLE[c] == -1) {
+            std::cerr << "Рядок " << lineNumber
+                      << ", символ " << i + 1
+                      << ": Некоректний вхідний символ ('" << c << "')\n";
+            return false;
+        }
+
+        val = (val << 6) + DECODE_TABLE[c];
+        valb += 6;
+
+        if (valb >= 0) {
+            output.push_back(char((val >> valb) & 0xFF));
+            valb -= 8;
+        }
+    }
+
+    return true;
+}
+
+int FileDecodeFunc(const std::string& inputFile, const std::string& outputFileUser) {
+
+    std::ifstream in(inputFile);
+    if (!in) {
+        std::cerr << "Cannot open input file\n";
+        return 1;
+    }
+
+    std::string outputFile = outputFileUser;
+    if (outputFile.empty()) {
+        if (inputFile.size() > 7 &&
+            inputFile.substr(inputFile.size() - 7) == ".base64") {
+            outputFile = inputFile.substr(0, inputFile.size() - 7);
+            std::cout << "Suggested output file: " << outputFile << "\n";
+        } else {
+            outputFile = inputFile + ".decoded";
+            std::cout << "Suggested output file: " << outputFile << "\n";
+        }
+
+        std::cout << "Press ENTER to accept or enter new name: ";
+        std::string temp;
+        std::getline(std::cin, temp);
+        if (!temp.empty())
+            outputFile = temp;
+    }
+
+    std::vector<unsigned char> decodedData;
+    std::string line;
+    int lineNumber = 0;
+    bool paddingEncountered = false;
+    bool finished = false;
+
+    while (std::getline(in, line)) {
+        lineNumber++;
+
+        if (line.empty())
+            continue;
+
+        if (line[0] == '-')
+            continue;
+
+        if (finished) {
+            std::cerr << "Наявні дані після кінця повідомлення\n";
+            break;
+        }
+
+
+        if (line.length() > 76) {
+            std::cerr << "Рядок " << lineNumber
+                << ": Рядок занадто довгий ("
+                << line.length() << ")\n";
+            return 1;
+        }
+
+        if (line.length() % 4 != 0) {
+            std::cerr << "Рядок " << lineNumber
+                << ": Довжина Base64 рядка не кратна 4 ("
+                << line.length() << ")\n";
+            return 1;
+        }
+
+        if (line.find('-') != std::string::npos && line[0] != '-') {
+            std::cerr << "Рядок " << lineNumber
+                << ": Некоректний вхідний символ ('-')\n";
+            return 1;
+        }
+
+        if (!decodeBase64Line(line, decodedData, lineNumber, paddingEncountered))
+            return 1;
+
+        if (paddingEncountered)
+            finished = true;
+    }
+
+    in.close();
+
+    std::ofstream out(outputFile, std::ios::binary);
+    if (!out) {
+        std::cerr << "Cannot open output file\n";
+        return 1;
+    }
+
+    out.write(reinterpret_cast<const char*>(decodedData.data()),
+              decodedData.size());
+
+    out.close();
+
+    std::cout << "Decoding finished. Output: " << outputFile << "\n";
+    return 0;
 }
 
 int FileChooseFunc(const std::string& inputFile, const std::string& outputFileUser) {
@@ -66,9 +210,12 @@ int FileChooseFunc(const std::string& inputFile, const std::string& outputFileUs
 }
 
 int main() {
+
+    initDecodeTable();
     while (true) {
         std::cout << "\n=== MENU ===\n";
         std::cout << "1) Base64 encode file\n";
+        std::cout << "2) Base64 decode file\n";
         std::cout << "9) Open .base64 file in nvim\n";
         std::cout << "0) Exit\n";
 
@@ -98,6 +245,15 @@ int main() {
             std::getline(std::cin, outputFile);
 
             FileChooseFunc(inputFile, outputFile);
+        }
+        else if (choose == 2) {
+            std::string inputFile, outputFile;
+            std::cout << "Enter encoded file name: ";
+            std::cin >> inputFile;
+            std::cin.ignore();
+            std::cout << "Enter output file name (or leave empty): ";
+            std::getline(std::cin, outputFile);
+            FileDecodeFunc(inputFile, outputFile);
         }
         else if (choose == 9) {
             std::string fileToOpen;
